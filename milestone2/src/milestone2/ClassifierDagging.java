91b26627d6bbfb0a3b4f951279fadbc19af90a83
Vineet <Vineet@Vineet.home>
2013-11-05 22:29:06 -0500

    dagging

--- milestone2/src/milestone2/ClassifierDagging.java
+++ milestone2/src/milestone2/ClassifierDagging.java
@@ -15,20 +15,27 @@ import weka.core.converters.ArffLoader.ArffReader;
 public class ClassifierDagging {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader("data/anneal_train.arff"));
+        BufferedReader reader = new BufferedReader(new FileReader("data/autos_train.arff"));
         ArffReader arff = new ArffReader(reader);
         Instances isTrainingSet = arff.getData();
         isTrainingSet.setClassIndex(isTrainingSet.numAttributes() - 1);
 
-        Classifier cModel = (Classifier)new Dagging();   
+        Classifier cModel = (Classifier)new Dagging(); 
+        String[] op = new String[4];
+   	    op[0] = "-P";
+   	    op[1] = "1e-14";
+   	    op[2] = "-L";
+	    op[3] = "1e-6";
+   	     	    
+   	    cModel.setOptions(op);
         cModel.buildClassifier(isTrainingSet);
 
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("anneal_dagging.model"));
+        /*ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("anneal_dagging.model"));
         oos.writeObject(cModel);
         oos.flush();
-        oos.close();
+        oos.close(); */
 
-        BufferedReader reader2 = new BufferedReader(new FileReader("data/anneal_test.arff"));
+        BufferedReader reader2 = new BufferedReader(new FileReader("data/autos_test.arff"));
         ArffReader arff2 = new ArffReader(reader2);
         Instances isTestSet = arff2.getData();
         isTestSet.setClassIndex(isTestSet.numAttributes() - 1);
--- milestone2/src/milestone2/ClassifierSMO.java
+++ milestone2/src/milestone2/ClassifierSMO.java
@@ -15,17 +15,19 @@ import weka.core.converters.ArffLoader.ArffReader;
 public class ClassifierSMO {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader("data/audiology_train.arff"));
+        BufferedReader reader = new BufferedReader(new FileReader("data/balance-scale_train.arff"));
         ArffReader arff = new ArffReader(reader);
         Instances isTrainingSet = arff.getData();
         isTrainingSet.setClassIndex(isTrainingSet.numAttributes() - 1);
 
         Classifier cModel = (Classifier)new SMO();
-        String[] op = new String[4];
+        String[] op = new String[5];
    	    op[0] = "-P";
    	    op[1] = "1e-14";
    	    op[2] = "-L";
-   	    op[3] = "1e-6";
+   	    op[3] = "1e-4";
+   	    op[4] = "-M";
+   	    
    	    cModel.setOptions(op);
         cModel.buildClassifier(isTrainingSet);
                 
@@ -34,7 +36,7 @@ public class ClassifierSMO {
         oos.flush();
         oos.close();*/
 
-        BufferedReader reader2 = new BufferedReader(new FileReader("data/audiology_test.arff"));
+        BufferedReader reader2 = new BufferedReader(new FileReader("data/balance-scale_test.arff"));
         ArffReader arff2 = new ArffReader(reader2);
         Instances isTestSet = arff2.getData();
         isTestSet.setClassIndex(isTestSet.numAttributes() - 1);
