13db0f9fa36fa7656b123a1cdc24de77a97230d5
Luigi Leung <lleung@cmu.edu>
2013-12-09 21:21:42 -0500

    Update ms2 for ms6 report

--- milestone2/src/milestone2/ClassifierNB.java
+++ milestone2/src/milestone2/ClassifierNB.java
@@ -13,7 +13,11 @@ import weka.core.converters.ArffLoader.ArffReader;
 public class ClassifierNB {
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader("data/anneal_train.arff"));
+        String dataset_name = "arrhythmia";
+        String traindata_name = String.format("ms5_milestone5data/%s_train.arff", dataset_name);
+        String testdata_name = String.format("ms5_data5bnew/%s_test.arff", dataset_name);
+        
+        BufferedReader reader = new BufferedReader(new FileReader(testdata_name));
         ArffReader arff = new ArffReader(reader);
         Instances isTrainingSet = arff.getData();
         isTrainingSet.setClassIndex(isTrainingSet.numAttributes() - 1);
@@ -22,7 +26,7 @@ public class ClassifierNB {
         cModel.buildClassifier(isTrainingSet);
 
 
-        BufferedReader reader2 = new BufferedReader(new FileReader("data/anneal_train.arff"));
+        BufferedReader reader2 = new BufferedReader(new FileReader(traindata_name));
         ArffReader arff2 = new ArffReader(reader2);
         Instances isTestSet = arff2.getData();
         isTestSet.setClassIndex(isTestSet.numAttributes() - 1);
@@ -41,14 +45,12 @@ public class ClassifierNB {
         double[][] cmMatrix = eTest.confusionMatrix();
 
         // Print out the confusion matrix (from ianma.wordpress.com)
-        for(int row_i=0; row_i<cmMatrix.length; row_i++){
+        /*for(int row_i=0; row_i<cmMatrix.length; row_i++){
             for(int col_i=0; col_i<cmMatrix.length; col_i++){
                 System.out.print(cmMatrix[row_i][col_i]);
                 System.out.print("|");
             }
             System.out.println();
-        }        
-
+        }*/
     }
-
 }
